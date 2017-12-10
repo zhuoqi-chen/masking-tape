3f4f45a37863af728f17650df6fe391d27b444bc
zhuoqi-chen <zhuoqi_chen@126.com>
2017-12-10 23:06:41 +0800

    add validate plugin

--- service/app/controller/vol.js
+++ service/app/controller/vol.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
+const createRule = {
+  name: { type: 'string' },
+};
 class VolController extends Controller {
   async index() {
     const result = await this.ctx.service.vol.list();
     this.ctx.body = result;
   }
   async create() {
+    this.ctx.validate(createRule);
     const data = {
       name: this.ctx.request.body.name,
     };
--- service/config/plugin.js
+++ service/config/plugin.js
@@ -6,3 +6,8 @@ exports.mysql = {
   enable: true,
   package: 'egg-mysql',
 };
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+};
+
